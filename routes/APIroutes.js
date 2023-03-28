d6021065b6261518a4ab2e2f8a0a9b931c961da1
Patrick Ameglio <patrickameglio@Patricks-MacBook-Pro.local>
2023-03-28 18:13:37 -0400

    updated code to add the post route

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -13,13 +13,19 @@ router.post("/notes", (req, res) =>{
     const notes = fs.readFileSync(path.join(__dirname, '../db/db.json'),"utf-8");
     console.log("notes", notes);
     const parse = JSON.parse(notes);
+    parse.push(req.body);
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(parse));
     res.json(parse);
 })
 
-
-
-
-
+router.delete("/notes/:id", (req, res) =>{
+    const notes = fs.readFileSync(path.join(__dirname, '../db/db.json'),"utf-8");
+    console.log("notes", notes);
+    const parse = JSON.parse(notes);
+    parse.delete(req.id);
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(parse));
+    res.json(parse);
+})
 
 
 
